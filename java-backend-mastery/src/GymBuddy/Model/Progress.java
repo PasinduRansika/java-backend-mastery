c4e6a7c6b69ca19ba4e724cbe0f975c4e267d7c4
Pasindu Ransika <v.pasinduransika@email.com>
2026-04-10 14:33:46 +0530

    feat(Member): implement WeightEntry and Progress Classes

--- java-backend-mastery/src/GymBuddy/Model/Progress.java
+++ java-backend-mastery/src/GymBuddy/Model/Progress.java
@@ -1,4 +1,65 @@
 package GymBuddy.Model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Progress {
+    private double duration;
+    private String type;
+    private LocalDate date;
+
+    public Progress(double duration,String type,LocalDate date){
+        validateDuration(duration);
+        this.duration = duration;
+        validateType(type);
+        this.type = type;
+        validateDate(date);
+        this.date = date;
+    }
+
+    private void validateDuration(double duration){
+        if(duration < 0){
+            throw new IllegalArgumentException("Please Enter Valid Duration !");
+        }
+    }
+
+    private void validateType(String type){
+        if(type == null || type.isBlank()){
+            throw new IllegalArgumentException("Please Enter Valid Type !");
+        }
+    }
+
+    private void validateDate(LocalDate date){
+        Objects.requireNonNull(date,"Date Cannot be Null !");
+        if(date.isAfter(LocalDate.now())){
+            throw new IllegalArgumentException("Please Enter Valid Date !");
+        }
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public void setDuration(double duration) {
+        validateDuration(duration);
+        this.duration = duration;
+    }
+
+    public void setDate(LocalDate date) {
+        validateDate(date);
+        this.date = date;
+    }
+    public void setType(String type) {
+        validateType(type);
+        this.type = type;
+    }
 }
+
--- java-backend-mastery/src/GymBuddy/Model/WeightEntry.java
+++ java-backend-mastery/src/GymBuddy/Model/WeightEntry.java
@@ -1,4 +1,44 @@
 package GymBuddy.Model;
 
+import java.time.LocalDate;
+
 public class WeightEntry {
+    private double weight;
+    private LocalDate date;
+
+    public WeightEntry(double weight,LocalDate date) {
+        validateWeight(weight);
+        validateDate(date);
+    }
+
+    private void validateWeight(double weight) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("Enter Valid Weight !");
+        }
+        this.weight = weight;
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new NullPointerException("Date cannot be null !");
+        }
+        if (date.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("Enter Valid Date !");
+        }
+        this.date = date;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setWeight(double weight) {
+        validateWeight(weight);
+        this.weight = weight;
+    }
+
 }
